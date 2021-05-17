c1558b3e072658db4ff8b5e7de50ef6528ac82df
Sylvie <sylvie.z.lu@gmail.com>
2021-05-17 13:25:07 +0200

    starter w/ parcel & react

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MOUNT_NODE = document.getElementById('app');
+
+ReactDOM.render(
+  <div>
+    <h1>Hellooo</h1>
+  </div>, MOUNT_NODE
+)
